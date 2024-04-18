dc0b8c6facaeee0b51a90fdd4b507339e60e50ae
moon <mun9659@naver.com>
2024-04-18 14:58:02 +0900

    Class3 2문제 복습

--- src/class3/Q1012.java
+++ src/class3/Q1012.java
@@ -7,66 +7,71 @@ import java.util.StringTokenizer;
 
 public class Q1012 {
 
-    public static int n, m, k;
-    
-    public static int[][] field;
-    public static boolean[][] check;
-    public static int[] dirX = {-1, 1, 0, 0};
-    public static int[] dirY = {0, 0, -1, 1};
+    static StringBuilder sb;
+
+    static int m, n, k;
+    static int[][] field;
+    static boolean[][] visited;
+
+    static int[] dirX = new int[] {-1, 1, 0, 0};
+    static int[] dirY = new int[] {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        sb = new StringBuilder();
 
         int t = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < t; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
-            m = Integer.parseInt(st.nextToken()); // 배추밭 가로길이
-            n = Integer.parseInt(st.nextToken()); // 배추밭 세로길이
-            k = Integer.parseInt(st.nextToken()); // 배추가 심어져 있는 갯수
+
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
 
             field = new int[n][m];
-            check = new boolean[n][m];
+            visited = new boolean[n][m];
 
+            // 배추 심기
             for(int j = 0; j < k; j++) {
-                st = new StringTokenizer(br.readLine(), " ");
-                
+                st = new StringTokenizer(br.readLine());
+
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
 
-                field[y][x] = 1;
+                field[y][x]++;
             }
 
-            int result = 0;
+            // DFS 실행
+            int count = 0;
             for(int o = 0; o < n; o++) {
                 for(int p = 0; p < m; p++) {
-                    if(!check[o][p] && field[o][p] == 1) {
+                    if(!visited[o][p] && field[o][p] == 1) {
                         dfs(o, p);
-                        result++;
+                        count++;
                     }
                 }
             }
 
-            sb.append(result).append("\n");
+            sb.append(count).append("\n");
         }
         System.out.println(sb);
     }
 
     public static void dfs(int y, int x) {
-        check[y][x] = true;
+        visited[y][x] = true;
 
         for(int i = 0; i < 4; i++) {
             int dx = x + dirX[i];
             int dy = y + dirY[i];
 
-            if(rangeCheck(dy, dx) && !check[dy][dx] && field[dy][dx] == 1) 
+            if(rangeCheck(dy, dx) && !visited[dy][dx] && field[dy][dx] == 1) {
                 dfs(dy, dx);
+            }
         }
     }
 
     public static boolean rangeCheck(int y, int x) {
-        return (y >= 0 && y < n && x >= 0 && x < m);
+        return y >= 0 && y < n && x >= 0 && x < m;
     }
 }
--- src/class3/Q1260.java
+++ src/class3/Q1260.java
@@ -10,28 +10,31 @@ import java.util.StringTokenizer;
 public class Q1260 {
 
     static int n, m, v;
-    static StringBuilder sb = new StringBuilder();
-    
-    static boolean[] visited;
+    static StringBuilder sb;
+
     static int[][] nodes;
+    static boolean[] visited;
     static Queue<Integer> queue;
 
     public static void main(String[] args) throws IOException {
+        // DFS와 BFS
+        // DFS: 깊이 우선 탐색
+        // BFS: 너비 우선 탐색
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
+        
         n = Integer.parseInt(st.nextToken()); // 정점의 개수
         m = Integer.parseInt(st.nextToken()); // 간선의 개수
-        v = Integer.parseInt(st.nextToken()); // 탐색 시작의 정점 번호
+        v = Integer.parseInt(st.nextToken()); // 탐색 시작할 정점의 번호
 
         nodes = new int[n + 1][n + 1];
         visited = new boolean[n + 1];
-
         queue = new LinkedList<>();
 
         for(int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
 
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
@@ -42,20 +45,18 @@ public class Q1260 {
         dfs(v);
         sb.append("\n");
 
-        visited = new boolean[n + 1];
+        visited = new boolean[n + 1]; // 방문 숫자 초기화
         bfs(v);
 
         System.out.println(sb);
     }
 
     public static void dfs(int v) {
-        if(visited[v]) return;
-
         visited[v] = true;
 
         sb.append(v).append(" ");
-
-        for(int i = 1; i < nodes[v].length; i++) {
+        
+        for(int i = 0; i < n + 1; i++) {
             if(!visited[i] && (nodes[v][i] == 1 || nodes[i][v] == 1)) {
                 dfs(i);
             }
@@ -63,7 +64,8 @@ public class Q1260 {
     }
 
     public static void bfs(int v) {
-        queue.offer(v);
+        queue.add(v);
+
         visited[v] = true;
 
         sb.append(v).append(" ");
@@ -71,10 +73,10 @@ public class Q1260 {
         while(!queue.isEmpty()) {
             int num = queue.poll();
 
-            for(int i = 1; i < nodes[num].length; i++) {
+            for(int i = 0; i < n + 1; i++) {
                 if(!visited[i] && (nodes[num][i] == 1 || nodes[i][num] == 1)) {
                     visited[i] = true;
-                    queue.offer(i);
+                    queue.add(i);
                     sb.append(i).append(" ");
                 }
             }
