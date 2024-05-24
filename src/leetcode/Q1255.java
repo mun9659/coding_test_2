cd28cf3e8ccce7bf303450a11cbe8fd5bd0fe0cd
Mun Kyoung-min <mun9659@naver.com>
2024-05-24 22:46:40 +0900

    LeetCode Daily 1문제(Backtracking 다시 풀 예정)

---
+++ src/leetcode/Q1255.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+public class Q1255 {
+
+    public static void main(String[] args) {
+
+        String[] words = {"dog","cat","dad","good"};
+        char[] letters = {'a','a','c','d','d','d','g','o','o'};
+        int[] score = {1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0};
+
+        System.out.println(maxScoreWords(words, letters, score));
+    }
+
+    private static int maxScoreWords(String[] words, char[] letters, int[] score) {
+
+        int[] count = new int[26];
+        int[] lettersCount = new int[26];
+
+        for(char c : letters)
+            count[c - 'a']++;
+
+        int result = 0;
+        result = backtracking(words, score, count, lettersCount, 0, 0, result);
+
+        return result;
+    }
+
+    private static int backtracking(String[] words, int[] score, int[] count, int[] lettersCount, int pos, int temp, int result) {
+        for(int i = 0; i < 26; i++) {
+            if(lettersCount[i] > count[i]) return result;
+        }
+
+        result = Math.max(result, temp);
+
+        for(int i = pos; i < words.length; i++) {
+            for(char c : words[i].toCharArray()) {
+                lettersCount[c - 'a']++;
+                temp += score[c - 'a'];
+            }
+
+            result = backtracking(words, score, count, lettersCount, i + 1, temp, result);
+
+            for(char c : words[i].toCharArray()) {
+                lettersCount[c - 'a']--;
+                temp -= score[c - 'a'];
+            }
+        }
+
+        return result;
+    }
+
+}
