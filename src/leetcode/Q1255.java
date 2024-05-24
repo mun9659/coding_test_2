7e4b12940322a78d61e2c26031a59e7f8116bf13
Mun Kyoung-min <mun9659@naver.com>
2024-05-24 22:47:12 +0900

    LeetCode 수정

--- src/leetcode/Q1255.java
+++ src/leetcode/Q1255.java
@@ -4,6 +4,13 @@ public class Q1255 {
 
     public static void main(String[] args) {
 
+        // 1255. Maximum Score Words Formed by Letters
+        // Given a list of words, list of  single letters (might be repeating) and score of every character.
+        //
+        // Return the maximum score of any valid set of words formed by using the given letters (words[i] cannot be used two or more times).
+        //
+        // It is not necessary to use all characters in letters and each letter can only be used once. Score of letters 'a', 'b', 'c', ... ,'z' is given by score[0], score[1], ... , score[25] respectively.
+
         String[] words = {"dog","cat","dad","good"};
         char[] letters = {'a','a','c','d','d','d','g','o','o'};
         int[] score = {1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0};
