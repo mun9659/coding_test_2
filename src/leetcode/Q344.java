804800b914b124ba50398512f81b5f8826691ad6
Mun Kyoung-min <mun9659@naver.com>
2024-06-02 21:10:53 +0900

    LeetCode Daily, Easy 1문제

---
+++ src/leetcode/Q344.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q344 {
+
+    public static void main(String[] args) throws IOException {
+
+        // Q344. Reverse String
+        // Write a function that reverses a string. The input string is given as an array of characters s.
+        //
+        // You must do this by modifying the input array in-place with O(1) extra memory.
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //
+        // char[] s = br.readLine().toCharArray();
+        char[] s_arr = {'A',' ','m','a','n',',',' ','a',' ','p','l','a','n',',',' ','a',' ','c','a','n','a','l',':',' ','P','a','n','a','m','a'};
+
+        reverseString(s_arr);
+
+        System.out.println(s_arr);
+    }
+
+    private static void reverseString(char[] s) {
+        for(int i = 0; i < s.length / 2; i++) {
+            char temp;
+
+            temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+    }
+}
